30eb85bb9ffd1f8573a512d14e9eae2fe5e9dd6f
Nursultan Kabylkas <kabylkas@gmail.com>
2018-10-23 09:06:23 -0700

    added x y offsets for different directions

--- src/SupportTypes.h
+++ src/SupportTypes.h
@@ -7,23 +7,30 @@ enum Direction {
   NORTH
 };
 
-class VisionOffsets {
-  private:
-    uint8_t x_offsets[14];
-    uint8_t y_offsets[14];
+/////////////////
+// f1 f2 f3 f4 f5
+// l2 p2 p3 p4 r2
+// l1 p1 XX p5 r1
+int north_x_offset[14];
+int north_y_offset[14];
+//                 f1  f2  f3  f4  f5  l1  l2  r1  r2  p1  p2  p3  p4  p5
+north_x_offsets = {-2, -1, 0,  1,  2,  -2, -2, 2,  2,  -1, -1, 0,  1,  1};
+north_y_offsets = {2,  2,  2,  2,  2,  0,  1,  0,  1,  0,  1,  1,  1,  0};
 
-  public:
-    VisionCoordinates(Direction facing) {
-      switch (facing) {
-        NORTH:
-          /////////////////
-          // f1 f2 f3 f4 f5
-          // l2 p2 p3 p4 r2
-          // l1 p1 XX p5 r1
+int east_x_offset[14];
+int east_y_offset[14];
+//                f1  f2  f3  f4  f5  l1  l2  r1  r2  p1  p2  p3  p4  p5
+east_x_offsets = {2,  2,  2,  2,  2,  0,  1,  0,  1,  0,  1,  1,   1,  0};
+east_y_offsets = {2,  1,  0,  -1, -2, 2,  2,  -2, -2, 1,  1,  0,  -1, -1};
 
-          //           f1  f2  f3  f4  f5  l1  l2  r1  r2  p1  p2  p3  p4  p5
-          x_offsets = {-2, -1, 0,  1,  2,  -2, -2, 2,  2,  -1, -1, 0,  1,  1};
-          y_offsets = {2,  2,  2,  2,  2,  0,  1,  0,  1,  0,  1,  1,  1,  0};
-      }
-    }
-};
+int south_x_offset[14];
+int south_y_offset[14];
+//                 f1  f2  f3  f4  f5  l1  l2  r1  r2  p1  p2  p3  p4  p5
+south_x_offsets = {2,  1,  0,  -1, -2, 2,  2,  -2, -2, 1,  1,  0,  -1, -1};
+south_y_offsets = {-2, -2, -2, -2, -2, 0,  -1, 0,  -1,  0, -1, -1, -1,  0};
+
+int west_x_offset[14];
+int west_y_offset[14]
+//                f1  f2  f3  f4  f5  l1  l2  r1  r2  p1  p2  p3  p4  p5
+west_x_offsets = {-2, -2, -2, -2, -2, 0,  -1, 0,  -1, 0,  -1, -1, -1, 0};
+west_y_offsets = {-2, -1, 0,  1,  2,  -2, -2, 2,  2,  -1, -1, 0,  1,  1};
